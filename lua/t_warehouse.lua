5373d989abf1be7b69a9a1303a9e83f53c3561c9
infuq <infuq217@outlook.com>
2024-06-24 17:29:49 +0800

    '...'

--- lua/t_warehouse.lua
+++ lua/t_warehouse.lua
@@ -9,6 +9,7 @@ updateValue["linkman"] = row["linkman"]
 
 local filter = {}
 filter["warehouseName"] = row["warehouse_name"]
+-- 二次开发, 类似 WHERE 条件
 updateValue["filter"] = filter
 
 if action == "update" then
